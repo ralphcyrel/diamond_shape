5e50dc4781d8c79bce6c8fa5cb1bdb7a088f8f89
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 12:19:00 +0800

    constructing of star

--- diamond_shape.py
+++ diamond_shape.py
@@ -3,9 +3,12 @@
 if n % 2 == 0:
     print("Input must be an integer" )
     return
-
-
 #Calculate the middle point
+mid = n // 2
 #Upper part of the diamond
+for i in range(mid + 1):
+    print(" " * (mid - i) + "*" * (2 * i + 1))
 #Lower part of the diamond
+for i in range(mid - 1, -1, -1):
+    print(" " * (mid - i) + "*" * (2 * i + 1))
 #Example usage of 5
