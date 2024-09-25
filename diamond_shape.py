74872c6ea2bb7cff9cbe4b9575ccc7dde958fb75
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 12:20:33 +0800

    constructing of star

--- diamond_shape.py
+++ diamond_shape.py
@@ -1,14 +1,17 @@
 #This code will make a diamond shape
+def print_diamond_shape(n):
 #Check if n is odd
-if n % 2 == 0:
-    print("Input must be an integer" )
-    return
+    if n % 2 == 0:
+        print("Input must be an integer" )
+        return
+
 #Calculate the middle point
-mid = n // 2
+    mid = n // 2
 #Upper part of the diamond
-for i in range(mid + 1):
-    print(" " * (mid - i) + "*" * (2 * i + 1))
+    for i in range(mid + 1):
+        print(" " * (mid - i) + "*" * (2 * i + 1))
 #Lower part of the diamond
-for i in range(mid - 1, -1, -1):
-    print(" " * (mid - i) + "*" * (2 * i + 1))
+    for i in range(mid - 1, -1, -1):
+        print(" " * (mid - i) + "*" * (2 * i + 1))
 #Example usage of 5
+print_diamong(5)
