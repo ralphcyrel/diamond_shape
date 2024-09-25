c0db4b5aafc20cea932a4e6e123b772518fd0211
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 12:16:17 +0800

    checking if the input is odd

--- diamond_shape.py
+++ diamond_shape.py
@@ -1,5 +1,10 @@
 #This code will make a diamond shape
 #Check if n is odd
+if n % 2 == 0:
+    print("Input must be an integer" )
+    return
+
+
 #Calculate the middle point
 #Upper part of the diamond
 #Lower part of the diamond
